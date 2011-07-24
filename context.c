6582f34832638b6f1fdb7a3f198ffef4b1b6e011
Moses Palmér <mosespalmer@gmail.com>
2011-07-24 14:32:58 +0200

    Corrected light position on x-axis

--- context.c
+++ context.c
@@ -343,7 +343,7 @@ lights_setup(Context *context, int enable)
 {
     if (enable) {
         GLfloat light_position[] = {
-            -2.0 * context->camera.ax,
+            2.0 * context->camera.ax,
             -2.0 * context->camera.ay,
             -1.0, 1.0};
         static GLfloat light_ambient[] = {0.0, 0.0, 0.0, 1.0};
